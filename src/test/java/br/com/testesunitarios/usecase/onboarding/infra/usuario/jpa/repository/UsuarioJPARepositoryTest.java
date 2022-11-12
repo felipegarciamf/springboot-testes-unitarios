53ff59c090a7ca088de81133b37beb139e0fdbd5
felipegarciamf <silasfelipegarcia@gmail.com>
2022-11-12 13:49:08 -0300

    subindo configuração de application para dev

--- src/test/java/br/com/testesunitarios/usecase/onboarding/infra/usuario/jpa/repository/UsuarioJPARepositoryTest.java
+++ src/test/java/br/com/testesunitarios/usecase/onboarding/infra/usuario/jpa/repository/UsuarioJPARepositoryTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -24,7 +25,8 @@ import br.com.testesunitarios.usecase.onboarding.infra.usuario.jpa.modelo.Usuari
 @ActiveProfiles("test")
 class UsuarioJPARepositoryTest {
 
-	
+	@Value("${teste}")
+	private String expiration;
 	
 	@Autowired
 	private UsuarioJPARepository usuarioJPARepository;
@@ -33,6 +35,9 @@ class UsuarioJPARepositoryTest {
 
 	@Test
 	void deveCadastrarUsuario() {
+		
+		System.out.println(expiration);
+		
 		Usuario usuario = new UsuarioBuilderTest().builder();
 		usuarioJPARepository.save(usuario);
 		assertThat(usuario.getCpf()).isEqualTo("40173586830");
