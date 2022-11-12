c480943dc68a789058ec806440963e57e1205b64
felipegarciamf <silasfelipegarcia@gmail.com>
2022-11-12 10:27:12 -0300

    subindo projeto

---
+++ src/main/java/br/com/testesunitarios/SpringBootTestesUntiariosApplication.java
@@ -0,0 +1,13 @@
+package br.com.testesunitarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestesUntiariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestesUntiariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/testesunitarios/SpringBootTestesUntiariosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.testesunitarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestesUntiariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
