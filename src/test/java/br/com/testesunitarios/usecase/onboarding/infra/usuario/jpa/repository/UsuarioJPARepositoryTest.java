2aa7ee9749439da80831e40f259994754c2a88b4
felipegarciamf <silasfelipegarcia@gmail.com>
2022-11-12 14:08:45 -0300

    ajustado o spring para junit 5

--- src/test/java/br/com/testesunitarios/usecase/onboarding/infra/usuario/jpa/repository/UsuarioJPARepositoryTest.java
+++ src/test/java/br/com/testesunitarios/usecase/onboarding/infra/usuario/jpa/repository/UsuarioJPARepositoryTest.java
@@ -1,32 +1,32 @@
 package br.com.testesunitarios.usecase.onboarding.infra.usuario.jpa.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import br.com.testesunitarios.builders.UsuarioBuilderTest;
 import br.com.testesunitarios.usecase.onboarding.infra.usuario.jpa.modelo.Usuario;
 
 
 
-@RunWith(SpringRunner.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
 class UsuarioJPARepositoryTest {
+	
+	
 
-	@Value("${teste}")
-	private String expiration;
+	
+	
+	@BeforeEach
+	public void init() {
+		
+	}
 	
 	@Autowired
 	private UsuarioJPARepository usuarioJPARepository;
@@ -35,9 +35,7 @@ class UsuarioJPARepositoryTest {
 
 	@Test
 	void deveCadastrarUsuario() {
-		
-		System.out.println(expiration);
-		
+				
 		Usuario usuario = new UsuarioBuilderTest().builder();
 		usuarioJPARepository.save(usuario);
 		assertThat(usuario.getCpf()).isEqualTo("40173586830");
